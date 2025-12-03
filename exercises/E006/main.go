c11094e942d02a0866065c9592714c7e61874ccb
Iagor Sousa <iagor.silva_iebt@123milhas.com.br>
2025-12-03 07:56:16 -0300

    feat(exercises): :tada: Making exercise 6

---
+++ exercises/E006/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	number := 42
+
+	fmt.Printf("Decimal: %d\n", number)
+	fmt.Printf("Binary: %b\n", number)
+	fmt.Printf("Hexadecimal: %x\n", number)
+}
